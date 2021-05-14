a137046d4338ae92ade59855bd32585ae0aa5c1f
lau-jr <laurentdeusdedith74@gmail.com>
2021-05-14 12:37:12 +0300

    route to add data from from file

--- mpis_backend/urls.py
+++ mpis_backend/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path, include
-from mpis_backend.views import SektaListView,  maoni, reply, send_message
+from mpis_backend.views import SektaListView,  maoni, reply, send_message, upload_data_from_file
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 app_name = 'mpis_backend'
 urlpatterns = [
     path('sekta/', SektaListView.as_view(), name='sekta-list'),
     path('maoni/', maoni, name='maoni'),
+    path('add-data/', upload_data_from_file, name='add-data'),
     path('reply/<pk>/', reply, name='reply'),
     path('send-message/<pk>/', send_message, name='send-msg'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
